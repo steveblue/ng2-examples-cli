f2fd58f0299fa043f29f487155da5f199833620f
Steve Belovarich <steveblue@gmail.com>
2016-05-27 21:30:53 -0700

    Added the ability to compile global scss => css in angular-cli-build.js

--- angular-cli-build.js
+++ angular-cli-build.js
@@ -1,20 +1,37 @@
+'use strict';
 /* global require, module */
 
 var Angular2App = require('angular-cli/lib/broccoli/angular2-app');
+var compileSass = require('broccoli-sass');
+var mergeTrees = require('broccoli-merge-trees');
+var _ = require('lodash');
+var glob = require('glob');
 
 module.exports = function(defaults) {
-  return new Angular2App(defaults, {
-    vendorNpmFiles: [
-      'systemjs/dist/system-polyfills.js',
-      'systemjs/dist/system.src.js',
-      'zone.js/dist/*.js',
-      'es6-shim/es6-shim.js',
-      'reflect-metadata/*.js',
-      'rxjs/**/*.js',
-      '@angular/**/*.js',
-      'd3/d3.js',
-      'three/build/three.js'
-      
-    ]
-  });
+  
+  let sourceDir = 'src';
+  let app = new Angular2App(defaults, {
+      sourceDir: sourceDir,
+      sassCompiler: {
+        includePaths: [
+          'public/style'
+        ]
+      },
+      vendorNpmFiles: [
+        'systemjs/dist/system-polyfills.js',
+        'systemjs/dist/system.src.js',
+        'zone.js/dist/*.js',
+        'es6-shim/es6-shim.js',
+        'reflect-metadata/*.js',
+        'rxjs/**/*.js',
+        '@angular/**/*.js',
+        'd3/d3.js',
+        'three/build/three.js'
+      ]
+    });
+    let styles = mergeTrees(_.map(glob.sync('public/**/*.scss'), function(sassFile) {
+        sassFile = sassFile.replace('public/', '');
+        return compileSass(['public'], sassFile, sassFile.replace(/.scss$/, '.css'));
+    }));
+    return mergeTrees([styles, app], { overwrite: true });
 };
